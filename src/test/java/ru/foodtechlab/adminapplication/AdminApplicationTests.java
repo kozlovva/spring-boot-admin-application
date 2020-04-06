ee0184c0af44fc47e34e9dee0fe3deb001da5fab
Владимир Козлов <kozlovva@sushivesla.team>
2020-04-06 09:40:10 +0400

    init

---
+++ src/test/java/ru/foodtechlab/adminapplication/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.foodtechlab.adminapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
